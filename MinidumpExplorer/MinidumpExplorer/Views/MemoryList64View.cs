5f608d226bdff4b334213166768663ed6c5179d9
Greg Nagel <GregTheDev@users.noreply.github.com>
2014-05-11 12:58:05 +0000

    Added Memory64 view to frontend

---
+++ MinidumpExplorer/MinidumpExplorer/Views/MemoryList64View.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DbgHelp.MinidumpFiles;
+
+namespace MinidumpExplorer.Views
+{
+    public partial class MemoryList64View : BaseViewControl
+    {
+        private MiniDumpMemoryDescriptor64[] _memoryList;
+
+        public MemoryList64View()
+        {
+            InitializeComponent();
+        }
+
+        public MemoryList64View(MiniDumpMemoryDescriptor64[] memoryList)
+            : this()
+        {
+            _memoryList = memoryList;
+
+            if (_memoryList.Length == 0)
+            {
+                this.listView1.Items.Add("No data found for stream");
+            }
+            else
+            {
+                foreach (MiniDumpMemoryDescriptor64 memoryRange in memoryList)
+                {
+                    ListViewItem newItem = new ListViewItem(memoryRange.StartOfMemoryRangeFormatted);
+                    newItem.SubItems.Add(memoryRange.EndOfMemoryRangeFormatted);
+                    newItem.SubItems.Add(memoryRange.DataSizePretty);
+
+                    this.listView1.Items.Add(newItem);
+                }
+            }
+        }
+    }
+}
