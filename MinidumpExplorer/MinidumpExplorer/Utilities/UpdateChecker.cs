7e14f005997afd3c288b0a759d11452441b687dd
gnagel_cp <SND\gnagel_cp>
2015-11-21 09:08:44 +0000

    Added notifications when new version is available

---
+++ MinidumpExplorer/MinidumpExplorer/Utilities/UpdateChecker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MinidumpExplorer.Utilities
+{
+    internal class UpdateChecker
+    {
+        public string CheckForUpdates(string feedUrl)
+        {
+            try
+            {
+                Atom10FeedFormatter formatter = new Atom10FeedFormatter();
+
+                XmlReaderSettings readerSettings = new XmlReaderSettings();
+                readerSettings.DtdProcessing = DtdProcessing.Parse;
+
+                XmlReader reader = XmlReader.Create(feedUrl, readerSettings);
+                if (!formatter.CanRead(reader)) return null;
+
+                formatter.ReadFrom(reader);
+
+                var currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+                var update = (from i in formatter.Feed.Items
+                              where new Version(i.Title.Text) > currentVersion
+                              orderby i.LastUpdatedTime descending
+                              select i).FirstOrDefault();
+
+                if (update != null)
+                    return update.Links.Single().Uri.ToString();
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("MinidumpExplorer: Update check failed: " + ex.ToString());
+
+                return null;
+            }
+        }
+    }
+}
