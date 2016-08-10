5f013c7d69139421c180c708d9f8f8e787833996
Greg Nagel <GregTheDev@users.noreply.github.com>
2016-08-10 12:18:57 +0000

    Updates to sumary view ui

--- MinidumpExplorer/MinidumpExplorer/Views/SummaryView.Designer.cs
+++ MinidumpExplorer/MinidumpExplorer/Views/SummaryView.Designer.cs
@@ -29,19 +29,18 @@
         private void InitializeComponent()
         {
             this.label5 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lblNumberOfStreams = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.lblDateTime = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.lblFlags = new System.Windows.Forms.Label();
-            this.lblMainModule = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.groupBox1.SuspendLayout();
+            this.lblAvailableStreamsHeading = new System.Windows.Forms.Label();
+            this.txtMainModule = new System.Windows.Forms.TextBox();
+            this.txtDateTime = new System.Windows.Forms.TextBox();
+            this.txtFlags = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOperatingSystem = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label5
@@ -49,99 +48,42 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(4, 10);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(120, 13);
+            this.label5.Size = new System.Drawing.Size(83, 13);
             this.label5.TabIndex = 0;
-            this.label5.Text = "Process Main Module:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 56);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Number of Streams";
-            // 
-            // lblNumberOfStreams
-            // 
-            this.lblNumberOfStreams.AutoSize = true;
-            this.lblNumberOfStreams.Location = new System.Drawing.Point(130, 56);
-            this.lblNumberOfStreams.Name = "lblNumberOfStreams";
-            this.lblNumberOfStreams.Size = new System.Drawing.Size(73, 13);
-            this.lblNumberOfStreams.TabIndex = 2;
-            this.lblNumberOfStreams.Text = "<unknown>";
+            this.label5.Text = "Target Process:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(4, 33);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.Size = new System.Drawing.Size(96, 13);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Date/Time:";
-            // 
-            // lblDateTime
-            // 
-            this.lblDateTime.AutoSize = true;
-            this.lblDateTime.Location = new System.Drawing.Point(130, 33);
-            this.lblDateTime.Name = "lblDateTime";
-            this.lblDateTime.Size = new System.Drawing.Size(73, 13);
-            this.lblDateTime.TabIndex = 4;
-            this.lblDateTime.Text = "<unknown>";
+            this.label3.Text = "Target Date/Time:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(4, 79);
+            this.label4.Location = new System.Drawing.Point(6, 79);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "Dump Flags:";
             // 
-            // lblFlags
-            // 
-            this.lblFlags.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblFlags.Location = new System.Drawing.Point(130, 79);
-            this.lblFlags.Name = "lblFlags";
-            this.lblFlags.Size = new System.Drawing.Size(774, 35);
-            this.lblFlags.TabIndex = 6;
-            this.lblFlags.Text = "<unknown>";
-            // 
-            // lblMainModule
-            // 
-            this.lblMainModule.AutoSize = true;
-            this.lblMainModule.Location = new System.Drawing.Point(130, 10);
-            this.lblMainModule.Name = "lblMainModule";
-            this.lblMainModule.Size = new System.Drawing.Size(73, 13);
-            this.lblMainModule.TabIndex = 8;
-            this.lblMainModule.Text = "<unknown>";
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.listView1);
-            this.groupBox1.Location = new System.Drawing.Point(7, 130);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(897, 368);
-            this.groupBox1.TabIndex = 9;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Available Streams";
-            // 
             // listView1
             // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.FullRowSelect = true;
-            this.listView1.Location = new System.Drawing.Point(3, 18);
+            this.listView1.Location = new System.Drawing.Point(7, 143);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(891, 347);
-            this.listView1.TabIndex = 9;
+            this.listView1.Size = new System.Drawing.Size(905, 424);
+            this.listView1.TabIndex = 10;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
@@ -160,22 +102,83 @@
             this.columnHeader3.Text = "Size";
             this.columnHeader3.Width = 138;
             // 
+            // lblAvailableStreamsHeading
+            // 
+            this.lblAvailableStreamsHeading.AutoSize = true;
+            this.lblAvailableStreamsHeading.Location = new System.Drawing.Point(6, 127);
+            this.lblAvailableStreamsHeading.Name = "lblAvailableStreamsHeading";
+            this.lblAvailableStreamsHeading.Size = new System.Drawing.Size(150, 13);
+            this.lblAvailableStreamsHeading.TabIndex = 11;
+            this.lblAvailableStreamsHeading.Text = "lblAvailableStreamsHeading";
+            // 
+            // txtMainModule
+            // 
+            this.txtMainModule.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMainModule.Location = new System.Drawing.Point(148, 10);
+            this.txtMainModule.Name = "txtMainModule";
+            this.txtMainModule.ReadOnly = true;
+            this.txtMainModule.Size = new System.Drawing.Size(771, 15);
+            this.txtMainModule.TabIndex = 12;
+            this.txtMainModule.Text = "<unknown>";
+            // 
+            // txtDateTime
+            // 
+            this.txtDateTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDateTime.Location = new System.Drawing.Point(148, 33);
+            this.txtDateTime.Name = "txtDateTime";
+            this.txtDateTime.ReadOnly = true;
+            this.txtDateTime.Size = new System.Drawing.Size(100, 15);
+            this.txtDateTime.TabIndex = 13;
+            this.txtDateTime.Text = "<unknown>";
+            // 
+            // txtFlags
+            // 
+            this.txtFlags.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFlags.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFlags.Location = new System.Drawing.Point(146, 79);
+            this.txtFlags.Multiline = true;
+            this.txtFlags.Name = "txtFlags";
+            this.txtFlags.ReadOnly = true;
+            this.txtFlags.Size = new System.Drawing.Size(756, 35);
+            this.txtFlags.TabIndex = 15;
+            this.txtFlags.Text = "<unknown>";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Target Operating System:";
+            // 
+            // txtOperatingSystem
+            // 
+            this.txtOperatingSystem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtOperatingSystem.Location = new System.Drawing.Point(148, 56);
+            this.txtOperatingSystem.Name = "txtOperatingSystem";
+            this.txtOperatingSystem.ReadOnly = true;
+            this.txtOperatingSystem.Size = new System.Drawing.Size(400, 15);
+            this.txtOperatingSystem.TabIndex = 17;
+            this.txtOperatingSystem.Text = "<unknown>";
+            // 
             // SummaryView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.lblMainModule);
-            this.Controls.Add(this.lblFlags);
+            this.Controls.Add(this.txtOperatingSystem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFlags);
+            this.Controls.Add(this.txtDateTime);
+            this.Controls.Add(this.txtMainModule);
+            this.Controls.Add(this.lblAvailableStreamsHeading);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.lblDateTime);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.lblNumberOfStreams);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label5);
             this.Name = "SummaryView";
-            this.Size = new System.Drawing.Size(919, 501);
-            this.groupBox1.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(919, 575);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,17 +187,17 @@
         #endregion
 
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label lblNumberOfStreams;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label lblDateTime;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lblFlags;
-        private System.Windows.Forms.Label lblMainModule;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label lblAvailableStreamsHeading;
+        private System.Windows.Forms.TextBox txtMainModule;
+        private System.Windows.Forms.TextBox txtDateTime;
+        private System.Windows.Forms.TextBox txtFlags;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOperatingSystem;
     }
 }
