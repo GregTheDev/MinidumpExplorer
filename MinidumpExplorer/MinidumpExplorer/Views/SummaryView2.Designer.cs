52c71d1a7be9222f455c411b546be89dfafe008a
gnagel_cp <SND\gnagel_cp>
2016-08-11 13:03:33 +0000

    New prototype for summary screen

---
+++ MinidumpExplorer/MinidumpExplorer/Views/SummaryView2.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace MinidumpExplorer.Views
+{
+    partial class SummaryView2
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblAvailableStreamsHeading = new System.Windows.Forms.Label();
+            this.txtMainModule = new System.Windows.Forms.TextBox();
+            this.txtDateTime = new System.Windows.Forms.TextBox();
+            this.txtFlags = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOperatingSystem = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblSeperator1 = new System.Windows.Forms.Label();
+            this.lblSeperator2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Main Module:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Date/Time:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Dump Flags:";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(29, 215);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(859, 346);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Stream";
+            this.columnHeader1.Width = 305;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Location (RVA)";
+            this.columnHeader2.Width = 102;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Size";
+            this.columnHeader3.Width = 138;
+            // 
+            // lblAvailableStreamsHeading
+            // 
+            this.lblAvailableStreamsHeading.AutoSize = true;
+            this.lblAvailableStreamsHeading.Location = new System.Drawing.Point(26, 199);
+            this.lblAvailableStreamsHeading.Name = "lblAvailableStreamsHeading";
+            this.lblAvailableStreamsHeading.Size = new System.Drawing.Size(150, 13);
+            this.lblAvailableStreamsHeading.TabIndex = 11;
+            this.lblAvailableStreamsHeading.Text = "lblAvailableStreamsHeading";
+            // 
+            // txtMainModule
+            // 
+            this.txtMainModule.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMainModule.Location = new System.Drawing.Point(132, 46);
+            this.txtMainModule.Name = "txtMainModule";
+            this.txtMainModule.ReadOnly = true;
+            this.txtMainModule.Size = new System.Drawing.Size(760, 15);
+            this.txtMainModule.TabIndex = 12;
+            this.txtMainModule.Text = "<unknown>";
+            // 
+            // txtDateTime
+            // 
+            this.txtDateTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDateTime.Location = new System.Drawing.Point(132, 138);
+            this.txtDateTime.Name = "txtDateTime";
+            this.txtDateTime.ReadOnly = true;
+            this.txtDateTime.Size = new System.Drawing.Size(700, 15);
+            this.txtDateTime.TabIndex = 13;
+            this.txtDateTime.Text = "<unknown>";
+            // 
+            // txtFlags
+            // 
+            this.txtFlags.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFlags.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFlags.Location = new System.Drawing.Point(130, 161);
+            this.txtFlags.Multiline = true;
+            this.txtFlags.Name = "txtFlags";
+            this.txtFlags.ReadOnly = true;
+            this.txtFlags.Size = new System.Drawing.Size(756, 35);
+            this.txtFlags.TabIndex = 15;
+            this.txtFlags.Text = "<unknown>";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Operating System:";
+            // 
+            // txtOperatingSystem
+            // 
+            this.txtOperatingSystem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtOperatingSystem.Location = new System.Drawing.Point(132, 69);
+            this.txtOperatingSystem.Name = "txtOperatingSystem";
+            this.txtOperatingSystem.ReadOnly = true;
+            this.txtOperatingSystem.Size = new System.Drawing.Size(400, 15);
+            this.txtOperatingSystem.TabIndex = 17;
+            this.txtOperatingSystem.Text = "<unknown>";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 21);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Target Process";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MinidumpExplorer.Properties.Resources.processes_5760;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MinidumpExplorer.Properties.Resources.processes_5760;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 111);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(25, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 21);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Minidump";
+            // 
+            // lblSeperator1
+            // 
+            this.lblSeperator1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSeperator1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeperator1.Location = new System.Drawing.Point(31, 37);
+            this.lblSeperator1.Name = "lblSeperator1";
+            this.lblSeperator1.Size = new System.Drawing.Size(857, 1);
+            this.lblSeperator1.TabIndex = 22;
+            this.lblSeperator1.Text = "label7";
+            // 
+            // lblSeperator2
+            // 
+            this.lblSeperator2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSeperator2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSeperator2.Location = new System.Drawing.Point(31, 129);
+            this.lblSeperator2.Name = "lblSeperator2";
+            this.lblSeperator2.Size = new System.Drawing.Size(857, 1);
+            this.lblSeperator2.TabIndex = 23;
+            // 
+            // SummaryView2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblSeperator2);
+            this.Controls.Add(this.lblSeperator1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOperatingSystem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFlags);
+            this.Controls.Add(this.txtDateTime);
+            this.Controls.Add(this.txtMainModule);
+            this.Controls.Add(this.lblAvailableStreamsHeading);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Name = "SummaryView2";
+            this.Size = new System.Drawing.Size(919, 575);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label lblAvailableStreamsHeading;
+        private System.Windows.Forms.TextBox txtMainModule;
+        private System.Windows.Forms.TextBox txtDateTime;
+        private System.Windows.Forms.TextBox txtFlags;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOperatingSystem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblSeperator1;
+        private System.Windows.Forms.Label lblSeperator2;
+    }
+}
