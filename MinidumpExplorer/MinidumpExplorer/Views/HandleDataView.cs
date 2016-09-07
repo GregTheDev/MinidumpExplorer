99a7a1ddd31c0f09b9ade17465cd00830a336bab
Greg Nagel <GregTheDev@users.noreply.github.com>
2016-09-07 05:35:11 +0000

    Added filtering & sorting to handle view

--- MinidumpExplorer/MinidumpExplorer/Views/HandleDataView.cs
+++ MinidumpExplorer/MinidumpExplorer/Views/HandleDataView.cs
@@ -7,29 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using DbgHelp.MinidumpFiles;
+using MinidumpExplorer.Controls;
 
 namespace MinidumpExplorer.Views
 {
     public partial class HandleDataView : BaseViewControl
     {
+        private const int COL_ID = 0;
+        private const int COL_HANDLE_TYPE = 1;
+        private const int COL_OBJECT = 2;
+
         public HandleDataView()
         {
             InitializeComponent();
+
+            listView1.SetFilteringForColumn(COL_HANDLE_TYPE, true);
+
+            listView1.ColumnSorter.SortColumn(COL_ID, listViewItem => (listViewItem.Tag as MiniDumpHandleDescriptor).HandleId);
         }
 
         public HandleDataView(MiniDumpHandleDescriptor[] handles)
             : this()
         {
+            List<ListViewItem> listItems = new List<ListViewItem>();
+
             foreach (MiniDumpHandleDescriptor handle in handles)
             {
-                ListViewItem newItem = new ListViewItem("0x" + handle.HandleId.ToString("x8"));
+                ListViewItem newItem = new ListViewItem(Formatters.FormatAsHex(handle.HandleId));
+                newItem.Tag = handle;
+
                 newItem.SubItems.Add(handle.TypeName);
                 newItem.SubItems.Add(handle.ObjectName);
 
-                newItem.Tag = handle;
-
-                this.listView1.Items.Add(newItem);
+                listItems.Add(newItem);
             }
+
+            listView1.AddItemsForFiltering(listItems);
         }
     }
 }
--- MinidumpExplorer/MinidumpExplorer/Views/HandleDataView.designer.cs
+++ MinidumpExplorer/MinidumpExplorer/Views/HandleDataView.designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView1 = new MinidumpExplorer.Controls.MinidumpListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -77,7 +77,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
+        private MinidumpExplorer.Controls.MinidumpListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
