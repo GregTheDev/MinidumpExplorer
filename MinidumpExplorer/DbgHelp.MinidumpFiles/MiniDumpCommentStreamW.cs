5c09754fc7372ea7dae99f1fd84a5d34248d111d
Greg Nagel <greg.nagel@gmail.com>
2018-12-22 16:47:20 +0200

    Added reading of CommentW stream to dbghelp

---
+++ MinidumpExplorer/DbgHelp.MinidumpFiles/MiniDumpCommentStreamW.cs
@@ -0,0 +1,20 @@
+﻿namespace DbgHelp.MinidumpFiles
+{
+    /// <summary>
+    /// The comment string that was written to the dump file.
+    /// </summary>
+    public class MiniDumpCommentStreamW
+    {
+        internal MiniDumpCommentStreamW()
+        {
+            this.Comment = null;
+        }
+
+        internal MiniDumpCommentStreamW(string comment)
+        {
+            this.Comment = comment;
+        }
+
+        public string Comment { get; private set; }
+    }
+}
